059ae50d74276425e4cee04d8a35810f697a5aec
K.R. Zentner <krzentner@gmail.com>
2023-09-17 18:46:18 -0700

    Move epoch logic to dataloader

--- src/outrl/ppo.py
+++ src/outrl/ppo.py
@@ -53,7 +53,6 @@ class PPO(LightningModule):
         self.vf_loss_coeff = cfg.get("vf_loss_coeff", 0.1)
 
         self.save_hyperparameters(ignore=["buffer", "sampler", "env_cons"])
-        self.epochs_since_sampling = self.epochs_per_policy_step
 
     def training_step(self, batch: Dict[str, torch.Tensor]):
         advantages = batch["advantages"].squeeze()
@@ -134,74 +133,69 @@ class PPO(LightningModule):
 
     def preprocess(self):
         self.logged_dataset = False
-        if (
-            self.epochs_since_sampling >= self.epochs_per_policy_step
-        ):
-            self.buffer.clear_all()
-            self.sampler.sample(
-                self.buffer, self.agent, timestep_target=self.steps_per_epoch
-            )
+        self.buffer.clear_all()
+        self.sampler.sample(
+            self.buffer, self.agent, timestep_target=self.steps_per_epoch
+        )
 
-            valids = self.buffer.valid_mask()
-            self.valid_mask = valids
+        valids = self.buffer.valid_mask()
+        self.valid_mask = valids
 
-            all_obs = torch.cat(
-                [
-                    self.buffer.buffers["observations"],
-                    torch.zeros(
-                        (self.buffer.n_episodes, 1) + self.env_spec.observation_shape
-                    ),
-                ],
-                dim=1,
-            )
-            # Fill in final (t + 1) observation
-            for i in range(self.buffer.n_episodes):
-                eps_len = self.buffer.episode_length_so_far[i]
-                try:
-                    all_obs[i, eps_len] = self.buffer.episode_data[i][
-                        "last_observation"
-                    ]
-                except KeyError:
-                    pass
+        all_obs = torch.cat(
+            [
+                self.buffer.buffers["observations"],
+                torch.zeros(
+                    (self.buffer.n_episodes, 1) + self.env_spec.observation_shape
+                ),
+            ],
+            dim=1,
+        )
+        # Fill in final (t + 1) observation
+        for i in range(self.buffer.n_episodes):
+            eps_len = self.buffer.episode_length_so_far[i]
+            try:
+                all_obs[i, eps_len] = self.buffer.episode_data[i][
+                    "last_observation"
+                ]
+            except KeyError:
+                pass
 
-            # Compute vf_returns, filter based on episode termination
-            with torch.no_grad():
-                vf_returns = self.agent.vf_forward(all_obs)
-            # Can't use valids mask, since vf_returns goes to t + 1
-            for i, episode_length in enumerate(self.buffer.episode_length_so_far):
-                # Everything after last valid observation should be empty
-                vf_returns[i, episode_length + 1 :] = 0.0
-                # zero vf_{t+1} in terminated episodes
-                if self.buffer.episode_data[i].get("terminated", False):
-                    vf_returns[i, episode_length] = 0.0
+        # Compute vf_returns, filter based on episode termination
+        with torch.no_grad():
+            vf_returns = self.agent.vf_forward(all_obs)
+        # Can't use valids mask, since vf_returns goes to t + 1
+        for i, episode_length in enumerate(self.buffer.episode_length_so_far):
+            # Everything after last valid observation should be empty
+            vf_returns[i, episode_length + 1 :] = 0.0
+            # zero vf_{t+1} in terminated episodes
+            if self.buffer.episode_data[i].get("terminated", False):
+                vf_returns[i, episode_length] = 0.0
 
-            rewards = self.buffer.buffers["rewards"].clone()
-            rewards[~valids] = 0.0
+        rewards = self.buffer.buffers["rewards"].clone()
+        rewards[~valids] = 0.0
 
-            advantages = compute_advantages(
-                discount=self.discount,
-                gae_lambda=self.gae_lambda,
-                vf_returns=vf_returns,
-                rewards=rewards,
-            )
+        advantages = compute_advantages(
+            discount=self.discount,
+            gae_lambda=self.gae_lambda,
+            vf_returns=vf_returns,
+            rewards=rewards,
+        )
 
-            self.discounted_returns = discount_cumsum(
-                rewards.unsqueeze(-1), discount=self.discount
-            )
-            assert len(self.discounted_returns.shape) == 2
-            self.buffer_dataloader.extra_buffers = {
-                "vf_returns": vf_returns[:, :-1],
-                "advantages": advantages,
-                "discounted_returns": self.discounted_returns,
-                "valid_rewards": rewards,
-            }
-            self.epochs_since_sampling = 0
-        else:
-            self.epochs_since_sampling += 1
+        self.discounted_returns = discount_cumsum(
+            rewards.unsqueeze(-1), discount=self.discount
+        )
+        assert len(self.discounted_returns.shape) == 2
+        self.buffer_dataloader.extra_buffers = {
+            "vf_returns": vf_returns[:, :-1],
+            "advantages": advantages,
+            "discounted_returns": self.discounted_returns,
+            "valid_rewards": rewards,
+        }
 
     def train_dataloader(self) -> DataLoader:
         self.buffer_dataloader = FragmentDataloader(
-            self.buffer, batch_size=self.minibatch_size, callback=self.preprocess
+            self.buffer, batch_size=self.minibatch_size, callback=self.preprocess,
+            cycles=self.epochs_per_policy_step,
         )
         return self.buffer_dataloader
 
@@ -231,4 +225,9 @@ def gym_ppo(cfg: DictConfig):
 
 
 if __name__ == "__main__":
-    gym_ppo(OmegaConf.from_cli())
+    cli_cfg = OmegaConf.from_cli()
+    if 'cfg' in cli_cfg:
+        cfg = OmegaConf.merge(cli_cfg, OmegaConf.load(cli_cfg['cfg']))
+    else:
+        cfg = cli_cfg
+    gym_ppo(cfg)
