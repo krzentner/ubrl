c6b33f77b66d8583fc4bf238282ccb5cf17e2308
K.R. Zentner <krzentner@gmail.com>
2024-07-25 20:46:34 -0700

    Switched shell.nix to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,27 +2,34 @@
   description = "Unbound Reinforcement Learning";
 
   inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      pythonVersion = "python310";
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    pythonVersion = "python310";
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
         devShells.default = pkgs.mkShell rec {
           buildInputs = [
             pkgs.python310
             pkgs.zlib
             pkgs.stdenv.cc.cc
-            pkgs.python310Packages.tensorboard
+            # pkgs.python310Packages.tensorboard
             pkgs.quarto
           ];
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
           QUARTO_PYTHON = "${toString ./.}/.venv/bin/python";
         };
-      };
+      }
+    );
 }
